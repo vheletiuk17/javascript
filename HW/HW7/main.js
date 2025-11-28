5632f9286e8297739f318f668b50ac8c21c0ae38
vhele <vheletiuk17@gmail.com>
2025-11-28 22:53:44 +0200

    finish 7tn task

--- HW/HW7/main.js
+++ HW/HW7/main.js
@@ -1,2 +1,100 @@
-let a  = 'В'
-a = 33
+class Users{
+    constructor(id, name, surname, email, phone){
+        this.id = id
+        this.name = name
+        this.surname = surname
+        this.email = email
+        this.phone = phone
+    }
+
+}
+
+const arr = []
+
+let u1 = new Users(1,'kokos', 'Pupkin', 'asd@asd', 380671234567);
+let u2 = new Users(2,'kokos', 'Pupkin', 'asd@asd', 380671234567);
+let u3 = new Users(3,'kokos', 'Pupkin', 'asd@asd', 380671234567);
+let u4 = new Users(4,'kokos', 'Pupkin', 'asd@asd', 380671234567);
+let u5 = new Users(5,'kokos', 'Pupkin', 'asd@asd', 380671234567);
+let u6 = new Users(6,'kokos', 'Pupkin', 'asd@asd', 380671234567);
+let u7 = new Users(7,'kokos', 'Pupkin', 'asd@asd', 380671234567);
+let u8 = new Users(8,'kokos', 'Pupkin', 'asd@asd', 380671234567);
+let u9 = new Users(9,'kokos', 'Pupkin', 'asd@asd', 380671234567);
+let u10 = new Users(10,'kokos', 'Pupkin', 'asd@asd', 380671234567);
+arr.push(u1,u2, u3, u4, u5, u6, u7, u8, u9,u10)
+
+console.log(arr);
+
+console.log(arr.filter(user => user.id % 2 === 0));
+
+console.log(arr.sort((a, b) => a.id - b.id));
+
+
+
+class Client {
+    constructor(id,name, surname, email, phone, order){
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+        this.order = order;
+    }
+}
+
+let arr1 = []
+
+let client = new Client(1,'dya', 'Pupkin', 'asd@asd', 380671234567, ['kokos','bread','butter', 'apples']);
+let client1 = new Client(2,'dya', 'Pupkin', 'asd@asd', 380671234567, ['kokos','bread','butter']);
+let client2 = new Client(3,'dya', 'Pupkin', 'asd@asd', 380671234567, ['kokos','butter']);
+arr1.push(client,client1,client2);
+console.log(arr1);
+
+let sort = arr1.sort((a,b) => a.order.length - b.order.length);
+console.log(sort);
+
+
+function CarCreator(model,producer,year,maxSpeed,volume) {
+    this.model = model;
+    this.producer = producer;
+    this.year = year;
+    this.maxSpeed = maxSpeed;
+    this.volume = volume;
+    this.driver =  null
+
+    this.drive = function (){
+        console.log(`Їдемо зі швидкістю ${this.maxSpeed}`)
+    }
+
+    this.info = function (){
+        console.log(`Model - ${this.model} Producer - ${this.producer} Year - ${this.year} Max Speed - ${this.maxSpeed} Engine Volume - ${this.volume}%`);
+    }
+    this.updateSpeed = function (newSpeed){
+        this.maxSpeed += newSpeed
+        console.log(`Speed - ${this.maxSpeed} `);
+    }
+    this.updateYear = function (newYear){
+        this.year = newYear
+        console.log(`Year - ${this.year}`);
+    }
+    this.addDriver = function (driver){
+        this.driver =  driver;
+        console.log(`Driver - ${this.driver.name} ${this.driver.exp}`);
+    }
+
+}
+let car = new CarCreator('Rs Q8', 'Audi', 2022 , 322, 3.0);
+car.info()
+car.updateSpeed(20);
+car.updateYear(2025);
+car.addDriver({name:'Vasya', exp: 5});
+
+console.log(car);
+
+
+
+
+
+
+
+
